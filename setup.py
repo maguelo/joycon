7f34b9ecda7ef803fce59273fba3b0f65cc04e13
Miguel <piprojectsms@gmail.com>
2020-01-29 00:01:18 +0100

    Update version

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@
 from setuptools import setup
 
 setup(name='joycon',
-      version='1.0',
+      version='1.0.1',
       description='Driver for Nintento joycon',
       author='Miguel Maldonado',
       author_email='miguelangelmal@gmail.com',
